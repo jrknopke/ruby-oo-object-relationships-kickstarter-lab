b47805fe59d298fe71af8552346833b960cdcba5
Joseph Knopke <josephknopke@gmail.com>
2020-12-28 13:42:14 -0600

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,23 @@
+class Backer
+   
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def backed_projects
+        projects = ProjectBacker.all.select{|project| project.backer == self}
+        projects.map{|project| project.project}
+    end
+end
